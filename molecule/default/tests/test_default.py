17b9491431a81c33c643c779914b25fb8a8021c3
John <OldCrowEW@users.noreply.github.com>
2020-04-27 00:35:34 -0400

    Improvements (#11)
    - clean up of various tasks and variables
    - consolidate tests
    - travis matrix tests for all supported OSs
    - add dnsmasq for service discovery via dns
    - remove support for non-systemd OS distros
    - add support for CentOS 8
    - add support for Ubuntu 20.04
    - remove support for Ubuntu 18.04

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,82 +9,105 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_consul_user(host):
-    user = host.user("consul")
+    consul_user = host.user("consul")
 
-    assert user.exists
-    assert user.group == "consul"
-    assert user.shell == "/sbin/nologin"
-    assert user.home == "/var/run/consul"
-
-
-def test_consul_bin_file(host):
-    f = host.file('/usr/sbin/consul')
-
-    assert f.exists
-    assert f.is_file
+    assert consul_user.exists
+    assert consul_user.group == "consul"
+    assert consul_user.shell == "/sbin/nologin"
+    assert consul_user.home == "/var/run/consul"
 
 
-def test_consul_ver(host):
-    consul_ver = host.check_output('/usr/sbin/consul --version')
-    assert re.match("^Consul v1.7.2", consul_ver)
+def test_consul_bin(host):
+    consul_bin = host.file('/usr/sbin/consul')
+
+    assert consul_bin.exists
+    assert consul_bin.is_file
+
+
+def test_consul_bin_ver(host):
+    consul_bin_ver = host.check_output('/usr/sbin/consul --version')
+    assert re.match("^Consul v1.7.2", consul_bin_ver)
 
 
 def test_consul_conf_dir(host):
-    f = host.file('/etc/consul')
+    consul_conf_dir = host.file('/etc/consul')
 
-    assert f.exists
-    assert f.is_directory
+    assert consul_conf_dir.exists
+    assert consul_conf_dir.is_directory
 
 
 def test_consul_config(host):
-    f = host.file('/etc/consul/consul.hcl')
+    consul_config = host.file('/etc/consul/consul.hcl')
 
-    assert f.exists
-    assert f.is_file
-    assert f.user == 'consul'
-    assert f.group == 'consul'
-    assert f.mode == 0o640
+    assert consul_config.exists
+    assert consul_config.is_file
+    assert consul_config.user == 'consul'
+    assert consul_config.group == 'consul'
+    assert consul_config.mode == 0o640
 
 
 def test_consul_confd_dir(host):
-    f = host.file('/etc/consul/conf.d')
+    consul_confd_dir = host.file('/etc/consul/conf.d')
 
-    assert f.exists
-    assert f.is_directory
-    assert f.user == 'consul'
-    assert f.group == 'consul'
+    assert consul_confd_dir.exists
+    assert consul_confd_dir.is_directory
+    assert consul_confd_dir.user == 'consul'
+    assert consul_confd_dir.group == 'consul'
 
 
-def test_consul_sysconfig(host):
-    f = host.file('/etc/sysconfig/consul')
+def test_consul_sys_defaults(host):
+    if host.system_info.distribution == "centos":
+        defaults = "/etc/sysconfig/consul"
+    elif host.system_info.distribution == "debian":
+        defaults = "/etc/default/consul"
 
-    assert f.exists
-    assert f.is_file
+        consul_sys_defaults = host(defaults)
+        assert consul_sys_defaults.exists
+        assert consul_sys_defaults.is_file
 
 
 def test_consul_data_dir(host):
-    f = host.file('/var/lib/consul')
+    consul_data_dir = host.file('/var/lib/consul')
 
-    assert f.exists
-    assert f.is_directory
+    assert consul_data_dir.exists
+    assert consul_data_dir.is_directory
 
 
 def test_consul_run_dir(host):
-    f = host.file('/var/run/consul')
+    consul_run_dir = host.file('/var/run/consul')
 
-    assert f.exists
-    assert f.is_directory
+    assert consul_run_dir.exists
+    assert consul_run_dir.is_directory
 
 
 def test_consul_systemd_file(host):
-    f = host.file('/lib/systemd/system/consul.service')
+    consul_systemd_file = host.file('/lib/systemd/system/consul.service')
 
-    assert f.exists
-    assert f.is_file
+    assert consul_systemd_file.exists
+    assert consul_systemd_file.is_file
 
 
 def test_consul_running_and_enabled(host):
-    service = host.service('consul')
+    consul_service = host.service('consul')
 
-    assert service.is_running
-    assert service.is_enabled
+    assert consul_service.is_running
+    assert consul_service.is_enabled
+
+
+def test_dnsmasq_install(host):
+    dnsmasq_pkg = host.package('dnsmasq')
+
+    assert dnsmasq_pkg.is_installed
+
+
+def test_dnsmasq_consul_config(host):
+    dnsmasq_consul_config = host.file('/etc/dnsmasq.d/10-consul')
+
+    assert dnsmasq_consul_config.is_file
+
+
+def test_dnsmasq_running_and_enabled(host):
+    dnsmasq_service = host.service('dnsmasq')
+
+    assert dnsmasq_service.is_running
+    assert dnsmasq_service.is_enabled
