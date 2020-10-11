4e07eee124313c00f821f84adee37ee5a47d8c48
John Favorite <john.favorite@gmail.com>
2020-10-11 19:16:43 -0400

    comment out dnsmasq specific tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -110,30 +110,30 @@ def test_consul_running_and_enabled(host):
     assert consul_service.is_enabled
 
 
-def test_dnsmasq_install(host):
-    dnsmasq_pkg = host.package('dnsmasq')
+# def test_dnsmasq_install(host):
+#     dnsmasq_pkg = host.package('dnsmasq')
 
-    assert dnsmasq_pkg.is_installed
-
-
-def test_dnsmasq_config(host):
-    dnsmasq_config = host.file('/etc/dnsmasq.conf')
-
-    assert dnsmasq_config.exists
-    assert dnsmasq_config.is_file
-    assert dnsmasq_config.mode == 0o644
+#     assert dnsmasq_pkg.is_installed
 
 
-def test_dnsmasq_consul_config(host):
-    dnsmasq_consul_config = host.file('/etc/dnsmasq.d/10-consul')
+# def test_dnsmasq_config(host):
+#     dnsmasq_config = host.file('/etc/dnsmasq.conf')
 
-    assert dnsmasq_consul_config.exists
-    assert dnsmasq_consul_config.is_file
-    assert dnsmasq_consul_config.mode == 0o666
+#     assert dnsmasq_config.exists
+#     assert dnsmasq_config.is_file
+#     assert dnsmasq_config.mode == 0o644
 
 
-def test_dnsmasq_running_and_enabled(host):
-    dnsmasq_service = host.service('dnsmasq')
+# def test_dnsmasq_consul_config(host):
+#     dnsmasq_consul_config = host.file('/etc/dnsmasq.d/10-consul')
 
-    assert dnsmasq_service.is_running
-    assert dnsmasq_service.is_enabled
+#     assert dnsmasq_consul_config.exists
+#     assert dnsmasq_consul_config.is_file
+#     assert dnsmasq_consul_config.mode == 0o666
+
+
+# def test_dnsmasq_running_and_enabled(host):
+#     dnsmasq_service = host.service('dnsmasq')
+
+#     assert dnsmasq_service.is_running
+#     assert dnsmasq_service.is_enabled
